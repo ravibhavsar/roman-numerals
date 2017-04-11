45c72bd52c86d55ee854ac82f721d32218ef9cc7
ravi <bhavsar_ravi@hotmail.com>
2017-04-12 00:42:41 +1000

    created romannumeric generator

--- src/RomanNumeralGenerator.php
+++ src/RomanNumeralGenerator.php
@@ -2,10 +2,13 @@
 
 namespace Larowlan\RomanNumeral;
 
+use SebastianBergmann\ObjectEnumerator\InvalidArgumentException;
+
 /**
  * Defines a class for generating roman numerals from integers.
  */
-class RomanNumeralGenerator {
+class RomanNumeralGenerator
+{
 
   /**
    * Generates a roman numeral from an integer.
@@ -18,8 +21,42 @@ class RomanNumeralGenerator {
    * @return string
    *   Roman numeral representing the passed integer.
    */
-  public function generate(int $number, bool $lowerCase = FALSE) : string {
-    return "Not implemented";
+  public function generate(int $number, bool $lowerCase = FALSE) : string
+  {
+
+    $lookup = array(
+        'M' => 1000,
+        'CM' => 900,
+        'D' => 500,
+        'CD' => 400,
+        'C' => 100,
+        'XC' => 90,
+        'L' => 50,
+        'XL' => 40,
+        'X' => 10,
+        'IX' => 9,
+        'V' => 5,
+        'IV' => 4,
+        'I' => 1
+    );
+
+    $result = '';
+    while ($number > 0) {
+      foreach ($lookup as $roman => $value) {
+        if ($number >= $value) {
+          $number -= $value;
+          $result .= $roman;
+          break;
+        }
+      }
+    }
+
+    /** return lower case string result if lowercase flag is true */
+    if ($lowerCase) {
+      return strtolower($result);
+    }
+
+    return $result;
   }
 
 }
